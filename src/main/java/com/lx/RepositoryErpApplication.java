b3229953a776d66e38760f4e659abfd9e5e1b0f5
lidada <ligelbl@foxmail.com>
2020-09-23 11:12:57 +0800

    RepositoryErp

---
+++ src/main/java/com/lx/RepositoryErpApplication.java
@@ -0,0 +1,13 @@
+package com.lx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepositoryErpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lx/RepositoryErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoryErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
